f51311d155e2c94884a444bcaa2e680e82009ad4
Thomas Rey <pro.thomas.rey@gmail.com>
2023-05-30 14:22:25 +0200

    Add files via upload
    test web site language

---
+++ docs/js/translate.js
@@ -0,0 +1,17 @@
+function ChangeLanguage(lang){
+    const data = langData[lang];
+    Object.keys(data).forEach(function (key){
+        let element = document.getElementById(key);
+        if (element) {
+            element.textContent = data[key];
+        }
+    })
+}
+
+fetch("lang.json").then(response => response.json()).then(data => {
+    const langData = data;
+
+    ChangeLanguage("en")
+}).catch(error => {
+    console.error("Une erreur s'est produite lors de la récupération des données JSON: ", error)
+})
