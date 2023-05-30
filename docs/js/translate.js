b435269faab6958e9e5c9a565bfe6a8e9205527b
Thomas Rey <pro.thomas.rey@gmail.com>
2023-05-30 14:47:02 +0200

    Add files via upload

--- docs/js/translate.js
+++ docs/js/translate.js
@@ -7,13 +7,15 @@ function ChangeLanguage(lang){
         if (element) {
             element.textContent = data[key];
         }
-    })
+    });
 }
 
-fetch("lang.json").then(response => response.json()).then(data => {
+fetch("lang.json")
+  .then(response => response.json())
+  .then(data => {
     langData = data;
-
-    ChangeLanguage("en")
-}).catch(error => {
-    console.error("Une erreur s'est produite lors de la récupération des données JSON: ", error)
-})
+    ChangeLanguage("en");
+  })
+  .catch(error => {
+    console.error("Une erreur s'est produite lors de la récupération des données JSON: ", error);
+  });
