d4e7153e238a4d21056806b59765f047c054bb50
Thomas Rey <pro.thomas.rey@gmail.com>
2023-06-08 15:15:59 +0200

    Add files via upload

--- docs/js/tooltip.js
+++ docs/js/tooltip.js
@@ -2,3 +2,10 @@ let tooltipTriggerList = [].slice.call(document.querySelectorAll(`[data-bs-toggl
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+let progressList = [].slice.call(document.querySelectorAll(`[aria-valuemax="100"]`))
+progressList.forEach(progress => {
+    if (progress.getAttribute('aria-valuenow') !== '100') {
+        progress.classList.add("placeholder-wave");
+    }
+});
